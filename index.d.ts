41a1d50a4dae224136b148bc50e4fbd68fde3e39
Almanov Nikita <131481562+nikkeyl@users.noreply.github.com>
2024-08-09 12:14:51 +0300

    refactor: add compile files in root

---
+++ index.d.ts
@@ -0,0 +1,12 @@
+import { Config } from 'prettier';
+
+/**
+ * Define an Prettier config.
+ *
+ * @param {object} config Prettier config.
+ *
+ * @returns Prettier config.
+ */
+declare const defineConfig: (config: Config) => Config;
+
+export { defineConfig };
